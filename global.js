ee9f4e97e5b330ea21a1b3b89e4b7c8603084ef4
Haowen Zhang <haz099@ucsd.edu>
2026-04-14 18:32:30 -0700

    step2

--- global.js
+++ global.js
@@ -3,3 +3,28 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+
+let navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+currentLink?.classList.add("current");
+
+// const navLinks = $$("nav a");
+
+// navLinks.forEach(link => {
+//   if (link.host !== location.host) return;
+
+//   let linkPath = new URL(link.href).pathname;
+//   let currentPath = location.pathname;
+
+//   linkPath = linkPath.replace(/index\.html$/, "");
+//   currentPath = currentPath.replace(/index\.html$/, "");
+
+//   if (linkPath === currentPath) {
+//     link.classList.add("current");
+//   }
+// });
