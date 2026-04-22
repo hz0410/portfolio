8c823b322e0a80f7e17e5ddd3d4f0a298ccfcd0e
Haowen Zhang <haz099@ucsd.edu>
2026-04-22 13:58:14 -0700

    .

--- projects/projects.js
+++ projects/projects.js
@@ -4,13 +4,3 @@ const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
 
-
-
-
-const projects = await fetchJSON('../lib/projects.json');
-
-// Select title element
-const titleElement = document.querySelector('.projects-title');
-
-// Update text with project count
-titleElement.textContent = `Projects (${projects.length})`;
