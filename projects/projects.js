af2df94a2fd4fc5776e0b593dee9e7e039834c9e
Haowen Zhang <haz099@ucsd.edu>
2026-05-01 13:32:38 -0700

    ..

--- projects/projects.js
+++ projects/projects.js
@@ -2,17 +2,24 @@ import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
+renderProjects(projects, projectsContainer, 'h2');
+
+// Pie chart
 const svg = d3.select('#projects-pie-plot');
+
+let data = [1, 2];
+let colors = ['gold', 'purple'];
+
 let arcGenerator = d3.arc()
   .innerRadius(0)
   .outerRadius(50);
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI
-});
-svg.append('path')
-  .attr('d', arc)
-  .attr('fill', 'red');
-renderProjects(projects, projectsContainer, 'h2');
 
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+
+svg.selectAll('path')
+  .data(arcData)
+  .join('path')
+  .attr('d', arcGenerator)
+  .attr('fill', (d, i) => colors[i]);
 
