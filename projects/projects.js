7444e19fed30f7ea8e8aeb04e8a61da6e3e11a7e
Haowen Zhang <haz099@ucsd.edu>
2026-05-01 19:21:37 -0700

    ..

--- projects/projects.js
+++ projects/projects.js
@@ -7,14 +7,24 @@ renderProjects(projects, projectsContainer, 'h2');
 // Pie chart
 const svg = d3.select('#projects-pie-plot');
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' }
+];
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 let arcGenerator = d3.arc()
   .innerRadius(0)
   .outerRadius(50);
 
-let sliceGenerator = d3.pie();
+// let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie()
+  .value(d => d.value);
+
 let arcData = sliceGenerator(data);
 
 svg.selectAll('path')
@@ -24,3 +34,13 @@ svg.selectAll('path')
 //   .attr('fill', (d, i) => colors[i]);
   .attr('fill', (d, i) => colors(i));
 
+let legend = d3.select('.legend');
+
+data.forEach((d, idx) => {
+  legend.append('li')
+    .attr('style', `--color:${colors(idx)}`)
+    .html(`
+      <span class="swatch"></span>
+      ${d.label} <em>(${d.value})</em>
+    `);
+});
