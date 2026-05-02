173abfd2a20b824126a2e07fe82797195c055a11
Haowen Zhang <haz099@ucsd.edu>
2026-05-01 20:31:38 -0700

    .

--- projects/projects.js
+++ projects/projects.js
@@ -1,20 +1,16 @@
 import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 const projects = await fetchJSON('../lib/projects.json');
+
+let sliceGenerator = d3.pie()
+  .value(d => d.value);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
 // Pie chart
 const svg = d3.select('#projects-pie-plot');
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' }
-];
+
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 let arcGenerator = d3.arc()
@@ -44,3 +40,4 @@ data.forEach((d, idx) => {
       ${d.label} <em>(${d.value})</em>
     `);
 });
+
