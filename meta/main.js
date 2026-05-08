843cda32ce485cb2180c0489ee165702a2f19a48
Haowen Zhang <haz099@ucsd.edu>
2026-05-07 17:29:41 -0700

    .

--- meta/main.js
+++ meta/main.js
@@ -13,6 +13,39 @@ async function loadData() {
   return data;
 }
 
-let data = await loadData();
+// let data = await loadData();
 
 // console.log(data); // you can remove later
+function processCommits(data) {
+  return d3
+    .groups(data, d => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+
+      let { author, date, time, timezone, datetime } = first;
+
+      let ret = {
+        id: commit,
+        url: 'https://github.com/YOUR_USERNAME/YOUR_REPO/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length
+      };
+
+      // 🔴 THIS PART IS IMPORTANT (hidden property)
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        enumerable: false   // 👈 THIS is the key fix
+      });
+
+      return ret;
+    });
+}
+
+let data = await loadData();
+let commits = processCommits(data);
+console.log(commits);
